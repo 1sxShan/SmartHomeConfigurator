92a9777fe1b64e4dc06d62949d2bedfd148b58f6
1sxShan <rk1637065@gmail.com>
2026-02-22 16:11:21 +0100

    2nd push

---
+++ simulatore casa json/src/configuration/Casa.java
@@ -0,0 +1,13 @@
+package configuration;
+import home_appliances.*;
+
+import java.util.List;
+
+public class Casa {
+
+    String nome;
+    double costo; // kW/h
+    List<Elettrodomestico> elettrodomestici;
+
+
+}
